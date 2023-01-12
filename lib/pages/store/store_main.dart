c4849ca44647d70bf530ad316c9dafb04fa99c02
yongyeonkim <es-kyy@smiltegate.com>
2023-01-12 23:16:44 +0900

    스토어 앱 완성 기능 구현은 없음. UI구성만 해보고 끝낸다. 기능 구현은 심화 과정에서 해보는걸로 한다. 오늘은 여기까지.

--- lib/pages/store/store_main.dart
+++ lib/pages/store/store_main.dart
@@ -20,36 +20,59 @@ class _StoreMainState extends State<StoreMain> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('스토어 앱'),
+        title: const Text('스토어 앱'),
+        elevation: 0, // appbar 그림자 삭제
       ),
       body: Container(
-        padding: EdgeInsets.all(12.0),
-        child: Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Text('Women'),
-                Text('Kids'),
-                Text('Shoes'),
-                Text('Bag'),
-              ],
-            ),
-            Expanded(
-              flex: 1,
-              child: Container(
-                color: Colors.lightBlueAccent,
-                child: Text('1111'),
+        padding: const EdgeInsets.all(12.0),
+        child: SafeArea( // statusbar(상태바) 영역에 padding(여백) 넣어준다.
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(25.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: const [
+                    Text(
+                      'Women',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      'Kids',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      'Shoes',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      'Bag',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                ),
               ),
-            ),
-            Expanded(
-              flex: 1,
-                child: Text('Image View')),
-          ],
+              Expanded(
+                child: Image.asset(
+                  "assets/bag.jpeg",
+                  fit: BoxFit.cover,
+                ),
+              ),
+              const SizedBox(
+                height: 2,
+              ),
+              Expanded(
+                child: Image.asset(
+                  "assets/cloth.jpeg",
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
       bottomNavigationBar: BottomNavigationBar(
-        items: [
+        items: const [
           BottomNavigationBarItem(
               icon: Icon(Icons.text_snippet), label: '나의 판매글'),
           BottomNavigationBarItem(icon: Icon(Icons.home), label: '홈'),
@@ -60,7 +83,7 @@ class _StoreMainState extends State<StoreMain> {
         onTap: _onItemTapped,
       ),
       floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
+        child: const Icon(Icons.add),
         onPressed: () {},
       ),
     );
